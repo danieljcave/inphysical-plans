385c640c1efa7d6431393644f2dc2ff4a925f72b
danieljcave <>
2023-12-18 05:05:28 +0000

    created post model and updated admin panel to be able to create posts within admin panel

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    image_upload = CloudinaryField('image', default='placeholder')
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return self.title
+
+    def total_likes(self):
+        return self.likes.count()
--- blog/views.py
+++ blog/views.py
@@ -3,35 +3,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    context = {
-        'post': post
-    }
-    return render(request, 'index.html', context)
-
-
-post = [
-    {
-        'author': 'Admin',
-        'title': 'Blog Post 1',
-        'content': 'This is the first blog post',
-        'date_created': '18th Dec, 2023'
-    },
-    {
-        'author': 'Admin2',
-        'title': 'Blog Post 2',
-        'content': 'This is the second blog post',
-        'date_created': '19th Dec, 2023'
-    },
-    {
-        'author': 'Admin3',
-        'title': 'Blog Post 3',
-        'content': 'This is the third blog post',
-        'date_created': '19th Dec, 2023'
-    },
-    {
-        'author': 'Admin4',
-        'title': 'Blog Post 4',
-        'content': 'This is the fourth blog post',
-        'date_created': '19th Dec, 2023'
-    }
-]
+    return render(request, 'index.html')
