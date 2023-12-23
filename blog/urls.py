ad1b0576f0319ff68cd2e379549c82676fd13ccb
danieljcave <>
2023-12-23 07:10:09 +0000

    Add an option for author of the post to update the title image or content. To check that the user that is updating the post using loginrequired mixing and users passes test mixen so no other user or guest can edit a post that he/she did not create

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<slug:slug>/', views.BlogPost.as_view(), name='blog_post'),
     path('like/<slug:slug>', views.LikePost.as_view(), name='post_like'),
     path('post/add/', views.CreatePost.as_view(), name="add_post"),
+    path('<slug:slug>/update/', views.UpdatePost.as_view(),
+         name='post_update'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Comment
 from django.http import HttpResponseRedirect
 from .forms import CommentForm, PostForm
@@ -60,6 +60,22 @@ class CreatePost(LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class UpdatePost(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    form_class = PostForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 class LikePost(LoginRequiredMixin, View):
