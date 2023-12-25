a210ba4ffdba2c00c3c8755d7a54ae4eb89cbb04
danieljcave <>
2023-12-25 03:27:56 +0000

    add comments to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,7 @@
+// Event listener for auto close message function using DOM Content Loaded
 document.addEventListener('DOMContentLoaded', alertTimeout);
 
+// Javascript function for auto close messages
 function alertTimeout() {
     setTimeout(function () {
         let messages = document.getElementById('msg');
