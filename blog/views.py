fb2d521d2f07658c514f9d61a1de7a836e30591e
danieljcave <>
2023-12-25 04:01:02 +0000

    add custom error pages to views and templates

--- blog/views.py
+++ blog/views.py
@@ -238,3 +238,31 @@ class LikePost(LoginRequiredMixin, View):
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('blog_post', args=[slug]))
+
+
+def handler403(request, exception):
+    """
+    Display custom 403 page
+    """
+    return render(request, "403.html", status=403)
+
+
+def handler404(request, exception):
+    """
+    Display custom 404 page
+    """
+    return render(request, "404.html", status=404)
+
+
+def handler405(request, exception):
+    """
+    Display custom 405 page
+    """
+    return render(request, "405.html", status=405)
+
+
+def handler500(request):
+    """
+    Display custom 500 page
+    """
+    return render(request, "500.html", status=500)
