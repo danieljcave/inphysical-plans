7fb447569a9c24db89490583b5a63aaca8a725ec
danieljcave <>
2023-12-25 01:44:03 +0000

    Add docstrings  to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,9 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    """
+    Database model for blog posts in admin panel
+    """
     summernote_fields = ('content')
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'slug', 'date_created')
@@ -13,5 +16,8 @@ class PostAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    """
+    Database model for comments in admin panel
+    """
     list_display = ('body', 'post', 'date_created',)
     search_fields = ('email', 'body')
