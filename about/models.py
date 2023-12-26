b55104bc70099c9f10e09fcca8d0bf44315d474b
danieljcave <>
2023-12-26 19:04:26 +0000

    fix an issue with delete comment and post modal not showing

--- about/models.py
+++ about/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -9,3 +11,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,11 @@
-// Event listener for auto close message function using DOM Content Loaded
+// // Event listener for auto close message function using DOM Content Loaded
 document.addEventListener('DOMContentLoaded', alertTimeout);
 
-// Javascript function for auto close messages
+// // Javascript function for auto close messages
 function alertTimeout() {
     setTimeout(function () {
         let messages = document.getElementById('msg');
         let alert = new bootstrap.Alert(messages);
         alert.close();
-    }, 3000);
+    }, 2500);
 }
