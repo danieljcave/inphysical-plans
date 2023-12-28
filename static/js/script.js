ce23d3f27d4edb27497e416e7e46e0d87eb036a4
danieljcave <>
2023-12-28 18:46:39 +0000

    add close out alert script.js

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+// // Event listener for auto close message function using DOM Content Loaded
+// // Javascript function for auto close messages
+document.addEventListener("DOMContentLoaded", (alertTimeoutalertTimeout) => {
+    setTimeout(function () {
+      $("#msg").alert("close");
+    }, 2500);
+  });
