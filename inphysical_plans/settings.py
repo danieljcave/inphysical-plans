2318d6932add79677dba903013dcdcd02e4907b6
danieljcave <>
2023-12-30 02:29:22 +0000

    deploy commit

--- inphysical_plans/settings.py
+++ inphysical_plans/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-danieljcave-inphysical-p-0ljkaned4a.us2.codeanyapp.com',
                  'inphysical-plans-e97fa8a6618e.herokuapp.com']
